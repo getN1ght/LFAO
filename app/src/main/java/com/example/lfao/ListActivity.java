524740d7a86e355f552547c044e024b45d074c47
malodetz <malo.detz@yandex.ru>
2022-10-19 15:06:44 +0300

    Add list addition

--- app/src/main/java/com/example/lfao/ListActivity.java
+++ app/src/main/java/com/example/lfao/ListActivity.java
@@ -23,7 +23,7 @@ public class ListActivity extends AppCompatActivity {
 
     private ListView listView;
 
-    private ArrayList<String> textList = new ArrayList<>(Arrays.asList("Milk", "Butter", "Bread"));
+    private ArrayList<String> textList = new ArrayList<>(Arrays.asList("Empty"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +36,14 @@ public class ListActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list_view);
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, textList.toArray(new String[0]));
+                android.R.layout.simple_list_item_1, android.R.id.text1, textList);
         listView.setAdapter(adapter);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textList.add("efefe");
-
+                adapter.notifyDataSetChanged();
             }
         });
     }
