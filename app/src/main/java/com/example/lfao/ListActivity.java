b79c95ee0e00f5f9f52814cd56f4a03ec7b4e5ec
Sergey Morozov <morozov.serg901@gmail.com>
2022-11-30 14:46:30 +0300

    Refactored list activity. Changed UI for list activity: now we do not need the Empty elenent in list to display it correctly

--- app/src/main/java/com/example/lfao/ListActivity.java
+++ app/src/main/java/com/example/lfao/ListActivity.java
@@ -3,11 +3,8 @@ package com.example.lfao;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
 import android.widget.*;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,7 +12,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.lfao.databinding.ActivityListBinding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -24,7 +21,7 @@ public class ListActivity extends AppCompatActivity {
 
     private ListView listView;
 
-    private ArrayList<String> textList = new ArrayList<>(Arrays.asList("Empty"));
+    private final ArrayList<String> textList = new ArrayList<>(Collections.singletonList("Empty"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,50 +32,42 @@ public class ListActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbar);
         getSupportActionBar().setTitle("Your list");
 
-        listView = (ListView) findViewById(R.id.list_view);
+        listView = findViewById(R.id.list_view);
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, textList);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
-                                    long id) {
-                textList.remove(position);
-                adapter.notifyDataSetChanged();
-            }
+        listView.setOnItemClickListener((parent, itemClicked, position, id) -> {
+            textList.remove(position);
+            adapter.notifyDataSetChanged();
         });
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                AlertDialog.Builder alert = new AlertDialog.Builder(ListActivity.this);
+        binding.fab.setOnClickListener(view -> {
+            AlertDialog.Builder alert = new AlertDialog.Builder(ListActivity.this);
 
 
-                final EditText edittext = new EditText(ListActivity.this);
-                alert.setTitle("Add item");
+            final EditText edittext = new EditText(ListActivity.this);
+            alert.setTitle("Add item");
 
-                alert.setView(edittext);
+            alert.setView(edittext);
 
-                alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String text = edittext.getText().toString();
-                        if (!text.isEmpty()){
-                            textList.add(text);
-                            adapter.notifyDataSetChanged();
-                        }
+            alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String text = edittext.getText().toString();
+                    if (!text.isEmpty()) {
+                        textList.add(text);
+                        adapter.notifyDataSetChanged();
                     }
-                });
+                }
+            });
 
-                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                    }
-                });
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                }
+            });
 
-                alert.show();
-            }
+            alert.show();
         });
     }
 
